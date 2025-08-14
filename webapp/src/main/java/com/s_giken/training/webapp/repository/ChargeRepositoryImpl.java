797dacae4c06fb96b712ba438293da1a29c3da4e
ho-nhat-phi <ho-nhat-phi@PC193.s-giken.local>
2025-08-14 14:09:50 +0900

    <DIV>タグを閉じる リポジトリのendDateがNULLだった場合の処理

--- webapp/src/main/java/com/s_giken/training/webapp/repository/ChargeRepositoryImpl.java
+++ webapp/src/main/java/com/s_giken/training/webapp/repository/ChargeRepositoryImpl.java
@@ -35,7 +35,8 @@ public class ChargeRepositoryImpl implements ChargeRepository {
 	    		charge.setChargeName((String)li.get("name"));
 	    		charge.setAmount((BigDecimal)li.get("amount"));
 	    		charge.setStartDate(((java.sql.Date)li.get("start_date")).toLocalDate());
-	    		charge.setEndDate(((java.sql.Date)li.get("end_date")).toLocalDate());
+	    		if(li.get("end_date") == null) {charge.setEndDate(null);}
+	    		else{charge.setEndDate(((java.sql.Date)li.get("end_date")).toLocalDate());}
 	    		charge.setCreatedAt((Timestamp)li.get("created_at"));
 	    		charge.setModifiedAt((Timestamp)li.get("modified_at"));
 	    		result.add(charge);
