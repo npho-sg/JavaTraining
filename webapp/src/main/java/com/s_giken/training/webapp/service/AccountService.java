66bb8d346ce5a8d5bf4f3c2d68ae24685df120ed
ho-nhat-phi <ho-nhat-phi@PC193.s-giken.local>
2025-09-18 14:13:53 +0900

    AccountService削除

--- webapp/src/main/java/com/s_giken/training/webapp/service/AccountService.java
+++
@@ -1,8 +0,0 @@
-package com.s_giken.training.webapp.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface AccountService {
-	
-	public UserDetails loadUserByUsername(String name);
-}
